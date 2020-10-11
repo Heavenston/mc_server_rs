605175a8e95ecec36703cd573c89141830d448df
Heavenstone <malolegendrelemaire@gmail.com>
2020-10-11 19:59:27 +0200

    Added C29EntityPosition

--- mc_networking/src/packets/client_bound.rs
+++ mc_networking/src/packets/client_bound.rs
@@ -426,6 +426,34 @@ mod play {
         }
     }
 
+    /// This packet is sent by the server when an entity moves less then 8 blocks;
+    /// if an entity moves more than 8 blocks C57EntityTeleport should be sent instead.
+    ///
+    /// https://wiki.vg/Protocol#Entity_Position
+    #[derive(Clone, Debug)]
+    pub struct C29EntityPosition {
+        pub entity_id: VarInt,
+        /// Change in X position as `(currentX * 32 - prevX * 32) * 128`
+        pub delta_x: i16,
+        /// Change in Y position as `(currentY * 32 - prevY * 32) * 128`
+        pub delta_y: i16,
+        /// Change in Z position as `(currentZ * 32 - prevZ * 32) * 128`
+        pub delta_z: i16,
+        pub on_ground: bool,
+    }
+    impl ClientBoundPacket for C29EntityPosition {
+        fn packet_id() -> i32 {
+            0x29
+        }
+        fn encode(&self, encoder: &mut PacketEncoder) {
+            encoder.write_varint(self.entity_id);
+            encoder.write_i16(self.delta_x);
+            encoder.write_i16(self.delta_y);
+            encoder.write_i16(self.delta_z);
+            encoder.write_bool(self.on_ground);
+        }
+    }
+
     /// Send information about the game
     ///
     /// https://wiki.vg/Pre-release_protocol#Join_Game
