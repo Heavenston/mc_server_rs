37300720cbf671fda6d35d59e4efe81b931b5370
Heavenstone <malolegendrelemaire@gmail.com>
2020-10-12 19:14:15 +0200

    Renamed some of the bitbuffer and added into_buffer

--- mc_networking/src/data_types/bitbuffer.rs
+++ mc_networking/src/data_types/bitbuffer.rs
@@ -1,34 +1,34 @@
 
 #[derive(Clone)]
 pub struct BitBuffer {
-    bits_per_entry: u64,
+    entry_bit_size: u64,
     entries_per_long: u64,
-    entries: usize,
+    size: usize,
     mask: u64,
     longs: Vec<u64>,
 }
 
 impl BitBuffer {
-    pub fn create(bits_per_entry: u8, entries: usize) -> BitBuffer {
-        let entries_per_long = 64 / bits_per_entry as u64;
+    pub fn create(entry_bit_size: u8, size: usize) -> BitBuffer {
+        let entries_per_long = 64 / entry_bit_size as u64;
         // Rounding up div
-        let longs_len = (entries + entries_per_long as usize - 1) / entries_per_long as usize;
+        let longs_len = (size + entries_per_long as usize - 1) / entries_per_long as usize;
         let longs = vec![0; longs_len];
         BitBuffer {
-            bits_per_entry: bits_per_entry as u64,
+            entry_bit_size: entry_bit_size as u64,
             longs,
-            entries,
+            size,
             entries_per_long,
-            mask: (1 << bits_per_entry) - 1,
+            mask: (1 << entry_bit_size) - 1,
         }
     }
 
     fn load(entries: usize, bits_per_entry: u8, longs: Vec<u64>) -> BitBuffer {
         let entries_per_long = 64 / bits_per_entry as u64;
         BitBuffer {
-            bits_per_entry: bits_per_entry as u64,
+            entry_bit_size: bits_per_entry as u64,
             longs,
-            entries,
+            size: entries,
             entries_per_long,
             mask: (1 << bits_per_entry) - 1,
         }
@@ -38,7 +38,7 @@ impl BitBuffer {
         // Find the set of indices.
         let arr_idx = word_idx / self.entries_per_long as usize;
         let sub_idx =
-            (word_idx as u64 - arr_idx as u64 * self.entries_per_long) * self.bits_per_entry;
+            (word_idx as u64 - arr_idx as u64 * self.entries_per_long) * self.entry_bit_size;
         // Find the word.
         let word = (self.longs[arr_idx] >> sub_idx) & self.mask;
         word as u32
@@ -48,9 +48,13 @@ impl BitBuffer {
         // Find the set of indices.
         let arr_idx = word_idx / self.entries_per_long as usize;
         let sub_idx =
-            (word_idx as u64 - arr_idx as u64 * self.entries_per_long) * self.bits_per_entry;
+            (word_idx as u64 - arr_idx as u64 * self.entries_per_long) * self.entry_bit_size;
         // Set the word.
         let mask = !(self.mask << sub_idx);
         self.longs[arr_idx] = (self.longs[arr_idx] & mask) | ((word as u64) << sub_idx);
+    }
+
+    pub fn into_buffer<T: From<u64>>(self) -> Vec<T> {
+        self.longs.into_iter().map(|i| T::from(i)).collect()
     }
 }
