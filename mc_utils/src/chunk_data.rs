ce28bb26ce11f0fbf66a1f2e535d46bfe27661e1
Heavenstone <malolegendrelemaire@gmail.com>
2020-10-31 22:47:09 +0100

    Use a vec instead of a HashMap in chunk_data

--- mc_utils/src/chunk_data.rs
+++ mc_utils/src/chunk_data.rs
@@ -2,50 +2,50 @@ use mc_networking::{
     data_types::bitbuffer::BitBuffer,
     packets::client_bound::{C20ChunkData, C20ChunkDataSection},
 };
-use std::collections::HashMap;
 
 #[derive(Clone)]
 pub struct ChunkDataSection {
-    blocks: [u16; 4096],
+    blocks: [usize; 4096],
+    palette: Vec<i32>,
 }
 impl ChunkDataSection {
     pub fn new() -> Self {
-        Self { blocks: [0; 4096] }
+        Self {
+            blocks: [0; 4096],
+            palette: vec![0],
+        }
     }
 
     pub fn set_block(&mut self, x: u8, y: u8, z: u8, block: u16) {
         let (x, y, z) = (x as usize, y as usize, z as usize);
-        self.blocks[x + (z * 16) + (y * 256)] = block;
+        if let Some((pb, _)) = self.palette.iter().enumerate().find(|(_, b)| **b == block as i32) {
+            self.blocks[x + (z * 16) + (y * 256)] = pb;
+        }
+        else {
+            self.blocks[x + (z * 16) + (y * 256)] = self.palette.len();
+            self.palette.push(block as i32);
+        }
     }
 
     pub fn get_block(&self, x: u8, y: u8, z: u8) -> u16 {
         let (x, y, z) = (x as usize, y as usize, z as usize);
-        self.blocks[x + (z * 16) + (y * 256)]
+        self.palette[self.blocks[x + (z * 16) + (y * 256)]] as u16
     }
 
     fn encode(&self) -> C20ChunkDataSection {
-        let mut palette_mapping: HashMap<i32, i32> = HashMap::new();
-        let mut palette: Vec<i32> = vec![];
-        for block in self.blocks.iter().map(|n| *n as i32) {
-            if !palette_mapping.contains_key(&block) {
-                palette_mapping.insert(block, palette.len() as i32);
-                palette.push(block);
-            }
-        }
-
         let mut block_count = 0;
 
-        let bits_per_block = ((palette.len() as f64).log2().ceil() as u8).max(4);
+        let bits_per_block = ((self.palette.len() as f64).log2().ceil() as u8).max(4);
         let mut blocks = BitBuffer::create(bits_per_block, 4096);
-        for (i, block) in self.blocks.iter().map(|n| *n as i32).enumerate() {
-            block_count += (block != 0) as i16;
-            blocks.set_entry(i, palette_mapping[&block] as u32);
+        for (i, pb) in self.blocks.iter().enumerate() {
+            block_count += (*pb != 0) as i16;
+            blocks.set_entry(i, *pb as u32);
         }
 
         C20ChunkDataSection {
             block_count,
             bits_per_block,
-            palette: Some(palette),
+            palette: Some(self.palette.clone()),
             data_array: blocks.into_buffer(),
         }
     }
@@ -53,29 +53,29 @@ impl ChunkDataSection {
 
 #[derive(Clone)]
 pub struct ChunkData {
-    sections: HashMap<u8, ChunkDataSection>,
+    sections: [Option<Box<ChunkDataSection>>; 17],
     // TODO: Make biomes mutable
     biomes: [i32; 1024],
 }
 impl ChunkData {
     pub fn new() -> Self {
         Self {
-            sections: HashMap::new(),
+            sections: [None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None],
             biomes: [1; 1024],
         }
     }
 
     /// Get a reference to a section, returns None id it doesn't exist
     pub fn get_section(&self, y: u8) -> Option<&ChunkDataSection> {
-        self.sections.get(&y)
+        self.sections[y as usize].as_ref().map(|ds| ds.as_ref())
     }
     /// Get a mutable reference to a section, create the section if it doesn't exist
     pub fn get_section_mut(&mut self, y: u8) -> &mut ChunkDataSection {
-        if !self.sections.contains_key(&y) {
-            self.sections.insert(y, ChunkDataSection::new());
+        if self.sections[y as usize].is_none() {
+            self.sections[y as usize] = Some(Box::new(ChunkDataSection::new()));
         }
 
-        self.sections.get_mut(&y).unwrap()
+        self.sections[y as usize].as_mut().map(|s| s.as_mut()).unwrap()
     }
 
     pub fn set_block(&mut self, x: u8, y: u8, z: u8, block: u16) {
@@ -98,7 +98,7 @@ impl ChunkData {
         let primary_bit_mask = {
             let mut primary_bit_mask = 0;
             for section in 0..16 {
-                if ((1 << section) & sections) != 0 && self.sections.contains_key(&section) {
+                if ((1 << section) & sections) != 0 && self.sections[section as usize].is_some() {
                     primary_bit_mask |= 1 << section;
                 }
             }
@@ -121,8 +121,8 @@ impl ChunkData {
         let chunk_sections = {
             let mut chunk_sections = vec![];
             for section_y in 0..=16 {
-                if self.sections.contains_key(&section_y) {
-                    chunk_sections.push(self.sections[&section_y].encode());
+                if self.sections[section_y as usize].is_some() {
+                    chunk_sections.push(self.sections[section_y as usize].as_ref().unwrap().encode());
                 }
             }
             chunk_sections
