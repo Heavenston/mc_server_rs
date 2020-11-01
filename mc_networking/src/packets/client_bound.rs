4cfeb47dc8817533312a160e2b61c0ebc9db72e9
Heavenstone <malolegendrelemaire@gmail.com>
2020-11-01 01:32:27 +0100

    fixed C3BMultiBlockChange encoding

--- mc_networking/src/packets/client_bound.rs
+++ mc_networking/src/packets/client_bound.rs
@@ -1080,7 +1080,7 @@ mod play {
 
     /// Fired whenever 2 or more blocks are changed within the same chunk on the same tick.
     ///
-    /// https://wiki.vg/Protocol#Multi_Block_Change
+    /// https://wiki.vg/Pre-release_protocol#Multi_Block_Change
     #[derive(Clone, Debug)]
     pub struct C3BMultiBlockChange {
         pub section_x: i32,
@@ -1097,8 +1097,14 @@ mod play {
         fn encode(&self, encoder: &mut PacketEncoder) {
             encoder.write_u64((((self.section_x as u64) & 0x3FFFFF) << 42) | ((self.section_y as u64) & 0xFFFFF) | (((self.section_z as u64) & 0x3FFFFF) << 20));
             encoder.write_bool(self.inverted_trust_edges);
+            encoder.write_varint(self.blocks.len() as VarInt);
             for block_change in self.blocks.iter() {
-                encoder.write_varlong((block_change.block_id as i64) << 12 | (block_change.x as i64) << 8 | (block_change.y as i64) << 4 | (block_change.z as i64));
+                encoder.write_varlong(
+                    (block_change.block_id as i64) << 12
+                        | (block_change.x as i64 & 0xF) << 8
+                        | (block_change.z as i64 & 0xF) << 4
+                        | (block_change.y as i64 & 0xF)
+                );
             }
         }
     }
