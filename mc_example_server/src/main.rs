b7c6f840434db85482388dcc08ca0257123136c8
Heavenstone <malolegendrelemaire@gmail.com>
2020-11-10 19:45:46 +0100

    Added cli arguments to mc_example_server

--- mc_example_server/src/main.rs
+++ mc_example_server/src/main.rs
@@ -4,10 +4,12 @@ mod server;
 
 use server::Server;
 
+use clap::{App, Arg};
 use fern::colors::{Color, ColoredLevelConfig};
+use log::*;
 use std::sync::Arc;
 
-fn setup_logger() {
+fn setup_logger(log_filter: log::LevelFilter) {
     let colors_line = ColoredLevelConfig::new()
         .debug(Color::BrightBlack)
         .info(Color::Green)
@@ -28,7 +30,7 @@ fn setup_logger() {
                 message = message,
             ))
         })
-        .level(log::LevelFilter::Debug)
+        .level(log_filter)
         .level_for("hyper", log::LevelFilter::Info)
         .level_for("reqwest", log::LevelFilter::Info)
         .level_for("mio", log::LevelFilter::Info)
@@ -39,10 +41,47 @@ fn setup_logger() {
 
 #[tokio::main]
 async fn main() {
-    setup_logger();
+    let clap_matches = App::new("Mc Example Server")
+        .author("Heavenstone")
+        .arg(
+            Arg::with_name("debug")
+                .short("d")
+                .long("debug")
+                .help("Enable debug logs"),
+        )
+        .arg(
+            Arg::with_name("port")
+                .short("p")
+                .long("port")
+                .value_name("port")
+                .default_value("25565")
+                .validator(|p| match p.parse::<i64>() {
+                    Err(..) => Err("Must be a valid number".to_string()),
+                    Ok(v) => {
+                        if v > 65353 {
+                            Err("Cannot be higher than 65353".to_string())
+                        }
+                        else {
+                            Ok(())
+                        }
+                    }
+                })
+                .takes_value(true),
+        )
+        .get_matches();
+
+    setup_logger(if clap_matches.is_present("debug") {
+        log::LevelFilter::Debug
+    }
+    else {
+        log::LevelFilter::Info
+    });
     let server = Arc::new(Server::new().await);
     Server::start_ticker(Arc::clone(&server)).await;
-    Server::listen(Arc::clone(&server), "0.0.0.0:25565")
-        .await
-        .unwrap();
+
+    let port = clap_matches.value_of("port").unwrap();
+
+    if let Err(error) = Server::listen(Arc::clone(&server), format!("0.0.0.0:{}", port)).await {
+        error!("Server stopped with error: {}", error);
+    }
 }
