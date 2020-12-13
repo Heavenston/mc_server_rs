495f13a9335ed449cd807e2acc0a99df4695a744
Heavenstone <malolegendrelemaire@gmail.com>
2020-12-13 19:11:53 +0100

    Added event handler positions

--- mc_events/src/lib.rs
+++ mc_events/src/lib.rs
@@ -6,21 +6,37 @@ pub use mc_events_macros::*;
 use dashmap::DashMap;
 use downcast_rs::{impl_downcast, Downcast};
 use std::any::TypeId;
+use smallvec::SmallVec;
 
 type ADashMap<K, V> = DashMap<K, V, ahash::RandomState>;
 
+/// Traits for Events
 pub trait Event: Downcast + Send + Sync {
     fn is_cancellable(&self) -> bool;
 }
 impl_downcast!(Event);
 
+/// Trait for event handlers
 pub trait EventHandler {
     fn event_type(&self) -> TypeId;
     fn on_event(&mut self, event: &mut dyn Event);
 }
 
+/// Define at which position the event handler should be called
+pub enum EventHandlerPosition {
+    /// The EventHandler will be added at the beginning of the handler list
+    First,
+    /// The EventHandler will be added at the end of the handler list
+    Last,
+    /// The EventHandler will always be called after all other handlers
+    Final,
+}
+
 pub struct EventManager {
-    pub handlers: ADashMap<TypeId, Vec<Box<dyn EventHandler>>>,
+    pub handlers: ADashMap<
+        TypeId,
+        (Vec<Box<dyn EventHandler>>, SmallVec<[Box<dyn EventHandler>; 2]>)
+    >,
 }
 impl EventManager {
     pub fn new() -> Self {
@@ -29,23 +45,37 @@ impl EventManager {
         }
     }
 
-    pub fn on<T: EventHandler + 'static>(&mut self, handler: T) {
+    /// Add an event handler with specified [EventHandlerPosition]
+    /// if [Option::None] is given than it will use [EventHandlerPosition:Last]
+    pub fn on<T: EventHandler + 'static>(&mut self, handler: T, position: Option<EventHandlerPosition>) {
+        let position = position.unwrap_or(EventHandlerPosition::Last);
+        let handler = Box::new(handler);
         let event_type = handler.event_type();
-        if let Some(mut handlers) = self.handlers.get_mut(&event_type) {
-            handlers.push(Box::new(handler))
+        let mut handlers = if let Some(handlers) = self.handlers.get_mut(&event_type) {
+            handlers
         }
         else {
-            self.handlers.insert(event_type, vec![Box::new(handler)]);
+            self.handlers.insert(event_type, (vec![], SmallVec::new()));
+            self.handlers.get_mut(&event_type).unwrap()
+        };
+        match position {
+            EventHandlerPosition::First => handlers.0.insert(0, handler),
+            EventHandlerPosition::Last => handlers.0.push(handler),
+            EventHandlerPosition::Final => handlers.1.push(handler),
         }
     }
 
+    /// Dispatch an event calling every event handler
     pub fn dispatch<E: Event>(&self, event: &mut E) {
         let event_type = TypeId::of::<E>();
         if !self.handlers.contains_key(&event_type) {
             return;
         }
 
-        for handler in self.handlers.get_mut(&event_type).unwrap().iter_mut() {
+        for handler in self.handlers.get_mut(&event_type).unwrap().0.iter_mut() {
+            handler.on_event(event);
+        }
+        for handler in self.handlers.get_mut(&event_type).unwrap().1.iter_mut() {
             handler.on_event(event);
         }
     }
--- mc_events/src/tests.rs
+++ mc_events/src/tests.rs
@@ -20,13 +20,47 @@ fn on_event(event: &mut EmptyEvent) {
 fn dispatching() {
     let mut event_manager = EventManager::new();
 
-    event_manager.on(OnEvent);
+    event_manager.on(OnEvent, None);
     let mut event = EmptyEvent("didn't work".to_string());
     event_manager.dispatch(&mut event);
     assert_eq!(event.0, "worked");
 
-    event_manager.on(OnEventTwo);
+    event_manager.on(OnEventTwo, None);
     event_manager.dispatch(&mut event);
 
     assert_eq!(event.0, "worked too!");
+}
+
+struct CounterEvent(u32);
+impl Event for CounterEvent {
+    fn is_cancellable(&self) -> bool {
+        false
+    }
+}
+
+struct PositionListener(u32);
+impl EventHandler for PositionListener {
+    fn event_type(&self) -> TypeId {
+        TypeId::of::<CounterEvent>()
+    }
+    fn on_event(&mut self, event: &mut dyn Event) {
+        let counter_event = event.downcast_mut::<CounterEvent>().unwrap();
+        counter_event.0 += 1;
+        assert_eq!(counter_event.0, self.0);
+    }
+}
+
+#[test]
+fn positions() {
+    let mut event_manager = EventManager::new();
+
+    event_manager.on(PositionListener(5), Some(EventHandlerPosition::Final));
+    event_manager.on(PositionListener(3), Some(EventHandlerPosition::Last));
+    event_manager.on(PositionListener(2), Some(EventHandlerPosition::First));
+    event_manager.on(PositionListener(6), Some(EventHandlerPosition::Final));
+    event_manager.on(PositionListener(4), Some(EventHandlerPosition::Last));
+    event_manager.on(PositionListener(1), Some(EventHandlerPosition::First));
+
+    let mut event = CounterEvent(0);
+    event_manager.dispatch(&mut event);
 }
