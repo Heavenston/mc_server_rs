b4ac30efe5c2421abd17c7a6b3f7c356daebd8e6
Heavenstone <malolegendrelemaire@gmail.com>
2020-12-23 19:46:08 +0100

    use the TickScheduler

--- mc_ecs_example_server/src/main.rs
+++ mc_ecs_example_server/src/main.rs
@@ -1,11 +1,11 @@
 mod chunk_loader;
 
-use mc_ecs_server_lib::{mc_schedule::McSchedule};
-
-use chunk_loader::StoneChunkProvider;
-use legion::{Schedule, World};
-
 use crate::chunk_loader::*;
+use chunk_loader::StoneChunkProvider;
+use mc_ecs_server_lib::mc_schedule::McSchedule;
+use mc_utils::tick_scheduler::{TickProfiler, TickScheduler};
+
+use legion::{Schedule, World};
 use std::{
     sync::{
         atomic::{AtomicUsize, Ordering},
@@ -15,7 +15,7 @@ use std::{
     time::{Duration, Instant},
 };
 
-fn ticker(tps_counter: Arc<AtomicUsize>) {
+fn main() {
     let mut world: World = World::default();
     let mut schedule = McSchedule::new();
 
@@ -27,35 +27,12 @@ fn ticker(tps_counter: Arc<AtomicUsize>) {
             .build(),
     );
 
-    loop {
-        schedule.tick(&mut world);
-        tps_counter.fetch_add(1, Ordering::Relaxed);
-        std::thread::yield_now();
-    }
-}
-
-fn main() {
-    let tps_counter = Arc::new(AtomicUsize::new(0));
-
-    std::thread::spawn({
-        let counter = tps_counter.clone();
-        move || {
-            ticker(counter);
-        }
-    });
-
-    let start = Instant::now();
-    let delay = Duration::from_secs(2);
-    let mut i = 0;
-    loop {
-        i += 1;
-        let ticks = tps_counter.load(Ordering::SeqCst);
-        tps_counter.store(0, Ordering::SeqCst);
-        let tps = ticks as f64 / delay.as_secs_f64();
-        println!("TPS: {:.0}", tps);
-        println!("MSPT: {:?}", Duration::from_secs_f64(1.0 / tps.max(0.01)));
-
-        let sleep_to = start + delay.checked_mul(i).unwrap();
-        sleep(sleep_to.saturating_duration_since(Instant::now()));
-    }
+    TickScheduler::builder()
+        .profiling_interval(Duration::from_secs(3))
+        .build().start(move || {
+            schedule.tick(&mut world);
+        }, Some(|profiler: &TickProfiler| {
+            println!("TPS: {:.0}", profiler.tick_per_seconds());
+            println!("DPT: {:?}", profiler.duration_per_tick());
+        }));
 }
